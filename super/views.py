967947a381d17dc78ad6339e7d6193e367cd36b4
unknown <dylanjlenz@gmail.com>
2022-08-30 10:17:18 -0500

    Updated Serializer in Super Get Function

--- super/views.py
+++ super/views.py
@@ -1,7 +1,11 @@
+from unittest import TextTestRunner
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializer import SuperSerializer
+from .models import Super
 
 @api_view(['GET'])
 def super_list(request):
-
-    return Response('ok')
+    super = Super.objects.all()
+    serializer = SuperSerializer(super, many=True)
+    return Response(serializer.data)
