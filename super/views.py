80e2b7816bc5749f51b35b464daeb5f7ac4360a0
unknown <dylanjlenz@gmail.com>
2022-08-30 16:51:06 -0500

    Updated Query Functionality in Super App, Views File

--- super/views.py
+++ super/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from super_type.models import Super_types
 from .serializer import SuperSerializer
 from .models import Supers
 
 @api_view(['GET', 'POST'])
 def super_list(request):
-    
     if request.method == 'GET':    
-        super = Supers.objects.all()
-        serializer = SuperSerializer(super, many=True)
+        type_name = request.query_params.get('type')
+        queryset = Supers.objects.all()
+        if type_name:
+            queryset = queryset.filter(super_type__type=type_name)
+        serializer = SuperSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -22,17 +25,14 @@ def super_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
     super = get_object_or_404(Supers, pk=pk)
-
     if request.method == 'GET':
         serializer = SuperSerializer(super)
         return Response(serializer.data)
-    
     elif request.method == 'PUT':
         serializer = SuperSerializer(super, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-
     elif request.method == 'DELETE':
         super.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
