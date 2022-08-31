e3cb3c5eb2550d31f3bc378439f038b5eabceb07
unknown <dylanjlenz@gmail.com>
2022-08-31 09:42:48 -0500

    Added Villain, Hero Functionality to Super, Get, View

--- super/views.py
+++ super/views.py
@@ -12,10 +12,17 @@ def super_list(request):
     if request.method == 'GET':    
         type_name = request.query_params.get('type')
         queryset = Supers.objects.all()
+        serializer = SuperSerializer(queryset, many=True)
         if type_name:
             queryset = queryset.filter(super_type__type=type_name)
-        serializer = SuperSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+            serializer = SuperSerializer(queryset, many=True)
+        elif type_name == 'Hero':
+            queryset = queryset.filter(super_type__type=type_name)
+            serializer = SuperSerializer(queryset, many=True)
+        elif type_name == 'Villain':
+            queryset = queryset.filter(super_type__type=type_name)
+            serializer = SuperSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -27,12 +34,12 @@ def super_detail(request, pk):
     super = get_object_or_404(Supers, pk=pk)
     if request.method == 'GET':
         serializer = SuperSerializer(super)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'PUT':
         serializer = SuperSerializer(super, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'DELETE':
         super.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
